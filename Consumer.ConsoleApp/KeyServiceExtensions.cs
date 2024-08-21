87eba2430a099245461451401635a270cecbbe80
Steve McKean <steven.mckean@peraton.com>
2024-08-21 08:05:54 -0500

    using microsoft di

---
+++ Consumer.ConsoleApp/KeyServiceExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Consumer.ConsoleApp;
+
+public static class KeyedServiceExtensions
+{
+    // Dictionary to store services by key
+    private static readonly Dictionary<string, Type> _serviceTypes = new();
+
+    public static IServiceCollection AddKeyedService<TService, TImplementation>(this IServiceCollection services, string key)
+        where TService : class
+        where TImplementation : class, TService
+    {
+        // Register the service with the DI container
+        services.AddTransient<TImplementation>();
+        _serviceTypes[key] = typeof(TImplementation);
+
+        return services;
+    }
+
+    public static TService GetKeyedService<TService>(this IServiceProvider serviceProvider, string key)
+        where TService : class
+    {
+        if (_serviceTypes.TryGetValue(key, out var serviceType))
+        {
+            return serviceProvider.GetService(serviceType) as TService;
+        }
+
+        throw new KeyNotFoundException($"Service with key '{key}' not found.");
+    }
+}
--- Consumer.ConsoleApp/Program.cs
+++ Consumer.ConsoleApp/Program.cs
@@ -1,29 +1,29 @@
 ﻿
 
 using Consumer.ConsoleApp;
-using Vax;
+using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
+services.AddSingleton<IConsoleWriter, ConsoleWriter>();
 
-// services.AddSingleton<IConsoleWriter, ConsoleWriter>();
+services.AddKeyedService<IdGenerator, IdGenerator>("IdGenerator");
+services.AddKeyedService<IIdGenerator, IdGenerator>("IdGenerator2");
 
-services.AddSingleton<ConsoleWriter>();
-
-services.AddSingleTon(provider => new IdGenerator( provider.GetService<ConsoleWriter>()));
+services.AddSingleton(provider => new IdGenerator( provider.GetService<IConsoleWriter>()));
 var serviceProvider = services.BuildServiceProvider();
 
-var service = serviceProvider.GetService<IdGenerator>();
-// var service2= serviceProvider.GetService<IIdGenerator>();
-// var service3 = serviceProvider.GetService<IIdGenerator>();
+var service = serviceProvider.GetKeyedService<IdGenerator>("IdGenerator");
+var service2= serviceProvider.GetKeyedService<IIdGenerator>("IdGenerator2");
+//var service3 = serviceProvider.GetService<IIdGenerator>();
 
-//Console.WriteLine(service.Id);
-// Console.WriteLine(service2.Id);
-// Console.WriteLine(service3.Id);
+Console.WriteLine(service.Id);
+Console.WriteLine(service2.Id);
+//Console.WriteLine(service3.Id);
 
 
 service?.PrintId();
-// service2?.PrintId();
-// service3?.PrintId();
+service2?.PrintId();
+//service3?.PrintId();
 
 Console.ReadLine();
 //service?.WriteLine("Hello from DI");
